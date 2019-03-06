208215d4820aca8dbd3645b55f47b8667d0783b5
Alexander <gapak.alex2017@yandex.ru>
2019-03-06 15:02:11 +0300

    Убран test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-json_list = [{'team_pass': 'AN7ptZ91', 'team_id': 476417196, 'done': True},
-             {'team_pass': '8so6Yq2S', 'team_id': 534567560, 'done': True},
-             {'team_pass': 'vbLqverx', 'team_id': 534257448, 'done': True}]
-
-if json_list[0]["done"] == True and json_list[1]["done"] == True and json_list[2]["done"] == True:
-    print("OK")
